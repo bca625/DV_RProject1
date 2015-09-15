3552f2d4a1dff5444e89eb1f90d36e147a04031d
Bryan A <bca625@gmail.com>
2015-09-15 18:20:35 -0500

    Added Data files

---
+++ 01 Data/Data1.R
@@ -0,0 +1,2 @@
+#Build first plot
+plot1 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_ba7433', PASS='orcl_ba7433', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
